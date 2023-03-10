2db2951392699ef8aee1da2d26bac8ee4ce5be45
Manol Trendafilov <manol@passion4.tech>
2023-03-10 18:45:18 +0200

    finish the positive guys

--- src/Sources/PositiveGuys.php
+++ src/Sources/PositiveGuys.php
@@ -8,6 +8,18 @@ class PositiveGuys
 
     public function getData(): array
     {
-        return [$this->title => 5000];
+        $contents = file_get_contents('https://raw.githubusercontent.com/man0l/api-statistics/main/data/visits.json');
+        $json = json_decode($contents, true);
+
+
+        $result = array_filter($json, function ($item) {
+            $now = new \DateTime();
+            $date = new \DateTime($item['date']);
+
+            return $date->format('Y-m') === $now->format('Y-m');
+        });
+
+        list($key) = array_keys($result);
+        return [$this->title => $result[$key]['visits']];
     }
 }
