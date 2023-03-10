f1c26b17a7f7a601db26975337ab9d66315af87a
Manol Trendafilov <manol@passion4.tech>
2023-03-10 15:05:33 +0200

    add placeholder data

--- src/App.php
+++ src/App.php
@@ -7,6 +7,24 @@ class App
 
     public function run(): void
     {
+        try {
+            echo json_encode(value: [
+                'error' => false,
+                'message' => "",
+                'data' => [
+                    "Google Analytics" => 150,
+                    "Positive Guys" => 5000
+                ],
+            ]);
+        } catch (\Exception $e) {
+            echo json_encode(value: [
+               'error' => true,
+               'message' => $e->getMessage(),
+               'data' => [
+
+               ]
+            ]);
+        }
 
     }
 }
